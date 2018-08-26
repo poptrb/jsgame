46da672ae45a32c010bf35b0c82b8e55085884ef
poptrb <popescuiulian67@gmail.com>
2018-08-26 13:30:50 +0300

    Serverul poate gazdui mai multe meciuri

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 var players = {};
+var usernames = {};
+var rooms = ['r1', 'r2'];
 var star = {
   x: Math.floor(Math.random() * 700) + 50,
   y: Math.floor(Math.random() * 500) + 50
@@ -19,52 +21,85 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  console.log('a user connected: ', socket.id);
-  // create a new player and add it to our players object
-  players[socket.id] = {
-    rotation: 0,
-    x: Math.floor(Math.random() * 700) + 50,
-    y: Math.floor(Math.random() * 500) + 50,
-    playerId: socket.id,
-    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
-  };
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-  // send the star object to the new player
-  socket.emit('starLocation', star);
-  // send the current scores
-  socket.emit('scoreUpdate', scores);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
+io.sockets.on('connection', function (socket) {
+  //socket.broadcast.emit('roomInfo', rooms);
+  socket.on('adduser', function (username, roomchoice) {
+    socket.join(roomchoice);
+    socket.username = username;
+    // Creeaza un nou jucator si il adauga la obiectul Players
+    players[socket.id] = {
+      rotation: 0,
+      t: 0,
+      x: Math.floor(Math.random() * 700) + 50,
+      y: Math.floor(Math.random() * 500) + 50,
+      playerId: socket.id,
+      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
+      room: roomchoice, //(Math.floor(Math.random() * 2) == 0) ? 'r1' : 'r2',
+      created: 0
+    };
+    console.log(players[socket.id].room);
+    socket.room = players[socket.id].room; //players[socket.id].team === 'red' ? 'r1' : 'r2';
+    console.log('a user connected: ', username, 'in room', socket.room);
+    usernames[username] = username;
+    
+    
+    //socket.to(socket.room).emit('currentPlayers', players, socket.room);
 
-  // when a player disconnects, remove them from our players object
+    // Trimite obiectul players noului jucator send the players object to the new player
+    io.sockets.in(socket.room).emit('currentPlayers', players, socket.room);
+    players[socket.id].created = 1;
+    // Trimite obiectul steluta 
+    io.sockets.in(socket.room).emit('starLocation', star);
+    // Trimite scorurile 
+    io.sockets.in(socket.room).emit('scoreUpdate', scores);
+    // Informeaza toti ceilalti jucatori de cel nou venit
+    //socket.broadcast.emit('newPlayer', players[socket.id]);
+    socket.broadcast.to(socket.room).emit('newPlayer', players[socket.id], socket.room);
+
+    
+  });
+  
+  // Cand un singur jucator se deconecteaza, ii eliminam din obiectul Players
   socket.on('disconnect', function () {
     console.log('user disconnected: ', socket.id);
+    socket.leave(socket.room);
     delete players[socket.id];
-    // emit a message to all players to remove this player
+    // Emite un mesaj catre toti jucatorii sa elimine acest jucator din meciul lor
     io.emit('disconnect', socket.id);
   });
 
-  // when a player moves, update the player data
+  // Cand un jucator se misca, updateaza datele acestuia pe server
   socket.on('playerMovement', function (movementData) {
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
     players[socket.id].rotation = movementData.rotation;
-    // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
+    players[socket.id].t = movementData.t;
+   
+    // Emite un mesaj catre toti jucatorii ca cineva s-a miscat
+    socket.to(socket.room).emit('playerMoved', players[socket.id]);
   });
 
+  socket.on('bulletFired', function (bulletData) {
+
+    socket.to(socket.room).emit('shotFired',bulletData);
+  });
+  /*socket.on('bulletMovement', function(positionData){
+
+  });*/
+  // Codul pentru 
   socket.on('starCollected', function () {
-    if (players[socket.id].team === 'red') {
+    if (players[socket.id].team === 'red') 
+    {
       scores.red += 10;
-    } else {
+    }
+    else 
+    {
       scores.blue += 10;
     }
     star.x = Math.floor(Math.random() * 700) + 50;
     star.y = Math.floor(Math.random() * 500) + 50;
-    io.emit('starLocation', star);
-    io.emit('scoreUpdate', scores);
+    io.sockets.in(socket.room).emit('starLocation', star);
+    io.sockets.in(socket.room).emit('scoreUpdate', scores);
   });
 });
 
